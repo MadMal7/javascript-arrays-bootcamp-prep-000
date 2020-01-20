51c9e1816f5834ab878a06e2c874c6a75cf4349f
Malachi Scott Prah <malachiprah@gmail.com>
2020-01-20 13:41:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,6 +22,7 @@ function accessElementInArray(array, index) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
+  return array
 }
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
